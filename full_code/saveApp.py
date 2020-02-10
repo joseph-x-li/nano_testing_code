0ca6a8776d75b98051070beb33c785893be67572
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-02-10 15:45:20 -0500

    file layout specs

--- full_code/saveApp.py
+++ full_code/saveApp.py
@@ -3,3 +3,11 @@ import pickle as pkl
 from tasks import add
 
 app = Celery('tasks', broker='amqp://jet2:1990@localhost:5672/master')
+
+
+#  (broker)
+#  main.py ---| -- celeryTasks.py has all method calls to Celery. Depends on Celery, and some nparray image saving library that has DOES NOT require openCV
+#             | -- csi_cam.py is camera class. Deps on OPENCV4.1.1
+
+#  (worker)
+#  celerTasks.py (copy)
