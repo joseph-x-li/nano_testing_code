17ad9e83a2947ea98a7c86024c29e9998def7211
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-03-02 13:30:53 -0500

    new test files

---
+++ serializationtest.py
@@ -0,0 +1,58 @@
+import numpy as np
+import time
+import pickle as pkl
+
+fakeFrame = np.random.randint(255, size=(1920, 1080, 3)) # make a fake image of proper size
+
+
+
+
+# pickle
+print("Now testing: msgpack-numpy")
+start = time.time()
+msgpack_enc = msgpack.packb(fakeFrame, default=m.encode)
+end = time.time()
+msgpack_enc_time = end-start
+start = time.time()
+msgpack_dec = msgpack.unpackb(x_enc, object_hook=m.decode)
+end = time.time()
+msgpack_dec_time = end-start
+print("Encoding time: {}\n Decoding time: {}".format(msgpack_enc_time, msgpack_dec_time)) 
+
+# msgpack_numpy
+print("Now testing: msgpack-numpy")
+start = time.time()
+msgpack_enc = msgpack.packb(fakeFrame, default=m.encode)
+end = time.time()
+msgpack_enc_time = end-start
+start = time.time()
+msgpack_dec = msgpack.unpackb(x_enc, object_hook=m.decode)
+end = time.time()
+msgpack_dec_time = end-start
+print("Encoding time: {}\n Decoding time: {}".format(msgpack_enc_time, msgpack_dec_time))
+
+# JSON
+print("Now testing: JSON")
+start = time.time()
+json_enc = json.dumps([str(nparray.dtype), base64.b64encode(nparray), nparray.shape])
+end = time.time()
+json_enc_time = end-start
+start = time.time()
+
+# get the encoded json dump
+enc = json.loads(json_enc)
+
+# build the numpy data type
+dataType = numpy.dtype(enc[0])
+
+# decode the base64 encoded numpy array data and create a new numpy array with this data & type
+dataArray = numpy.frombuffer(base64.decodestring(enc[1]), dataType)
+
+# if the array had more than one data set it has to be reshaped
+if len(enc) > 2:
+     dataArray.reshape(enc[2])   # return the reshaped numpy array containing several data sets
+
+end = time.time()
+json_dec_time = end-start
+print("Encoding time: {}\n Decoding time: {}".format(json_enc_time, json_dec_time))
+
