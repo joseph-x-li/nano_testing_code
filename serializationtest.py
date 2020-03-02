28437944fc78441121accf396def0c87922dbca4
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-03-02 13:36:12 -0500

    pickle

--- serializationtest.py
+++ serializationtest.py
@@ -8,13 +8,13 @@ fakeFrame = np.random.randint(255, size=(1920, 1080, 3)) # make a fake image of 
 
 
 # pickle
-print("Now testing: msgpack-numpy")
+print("Now testing: pickle")
 start = time.time()
-msgpack_enc = msgpack.packb(fakeFrame, default=m.encode)
+picke_enc = pickle.dumps(fakeFrame, protocol=0)
 end = time.time()
-msgpack_enc_time = end-start
+pickle_enc_time = end-start
 start = time.time()
-msgpack_dec = msgpack.unpackb(x_enc, object_hook=m.decode)
+pickle_dec = pickle.loads(picke_enc)
 end = time.time()
 msgpack_dec_time = end-start
 print("Encoding time: {}\n Decoding time: {}".format(msgpack_enc_time, msgpack_dec_time)) 
@@ -34,7 +34,7 @@ print("Encoding time: {}\n Decoding time: {}".format(msgpack_enc_time, msgpack_d
 # JSON
 print("Now testing: JSON")
 start = time.time()
-json_enc = json.dumps([str(nparray.dtype), base64.b64encode(nparray), nparray.shape])
+json_enc = json.dumps([str(fakeFrame.dtype), base64.b64encode(fakeFrame), fakeFrame.shape])
 end = time.time()
 json_enc_time = end-start
 start = time.time()
