b34528d3033a6fd54f36dc13245b46983837b913
jet2 <13826851+joseph-x-li@users.noreply.github.com>
2020-04-06 15:21:19 -0400

    move

--- full_code/zmq_rec_test.py
+++ full_code/zmq_rec_test.py
@@ -4,5 +4,6 @@ image_hub = imagezmq.ImageHub()
 while True:  # show streamed images until Ctrl-C
     frame_num, image = image_hub.recv_image()
     if image is not None:
+        print("{}".format(frame_num ))
         cv2.imwrite("images/frame{}.jpg".format(frame_num), image)
         image_hub.send_reply(b'OK')
