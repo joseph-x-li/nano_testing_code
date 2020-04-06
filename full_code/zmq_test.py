ac2a30b294eb1b87501444601b9e1fd3f18a7909
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-04-06 15:19:49 -0400

    more

--- full_code/zmq_test.py
+++ full_code/zmq_test.py
@@ -5,7 +5,7 @@ import imagezmq
 
 sender = imagezmq.ImageSender(connect_to='tcp://172.16.0.37:5555')
 
-JET1 = socket.gethostname() # send RPi hostname with each image
+# JET1 = socket.gethostname() # send RPi hostname with each image
 picam = csi_cam.csiCamera()
 time.sleep(2.0)  # allow camera sensor to warm up
 
@@ -17,4 +17,4 @@ for i in range(numFrames):
     img = camera.getFrame()
     while img is None:
         img = camera.getFrame()
-    sender.send_image(JET1, image)
+    sender.send_image(i, img)
---
+++ zmq_rec_test.py
@@ -0,0 +1,8 @@
+import cv2
+import imagezmq
+image_hub = imagezmq.ImageHub()
+while True:  # show streamed images until Ctrl-C
+    frame_num, image = image_hub.recv_image()
+    if image is not None:
+        cv2.imwrite("images/frame{}.jpg".format(frame_num), image)
+        image_hub.send_reply(b'OK')
