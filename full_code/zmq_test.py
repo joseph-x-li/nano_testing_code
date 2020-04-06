e9a4f6874f27c7f2ee173e71fc6169059a71e301
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-04-06 15:13:48 -0400

    zmq testing

---
+++ full_code/zmq_test.py
@@ -0,0 +1,20 @@
+import socket
+import time
+from imutils.video import VideoStream
+import imagezmq
+
+sender = imagezmq.ImageSender(connect_to='tcp://172.16.0.37:5555')
+
+JET1 = socket.gethostname() # send RPi hostname with each image
+picam = csi_cam.csiCamera()
+time.sleep(2.0)  # allow camera sensor to warm up
+
+print("start")
+
+numFrames = 5
+
+for i in range(numFrames):
+    img = camera.getFrame()
+    while img is None:
+        img = camera.getFrame()
+    sender.send_image(JET1, image)
