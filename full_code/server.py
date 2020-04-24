7f752f41606d5f871a13ec66c134da81ba5fae74
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-04-24 05:57:46 -0400

    fixes

--- full_code/server.py
+++ full_code/server.py
@@ -5,13 +5,13 @@ HEADERSIZE = 20
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(
-    (socket.gethostname(), 11234)
+    ("172.16.0.38", 11234)
 )
 
 s.listen(5)
 
 while True:
-    cluentsocket, address = s.accept()
+    clientsocket, address = s.accept()
     print(f"Connection from {address} is established")
     fakeFrame = np.random.randint(255, size=(1920, 1080, 3)) # make a fake image of proper size
     msg = fakeFrame.tobytes()
