6eb8d141af375eca6c295990dac08b5f3588bb72
jet2 <13826851+joseph-x-li@users.noreply.github.com>
2020-04-24 05:58:30 -0400

    fixes

--- full_code/client.py
+++ full_code/client.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import numpy as np
 
 HEADERSIZE = 20
 
@@ -12,16 +13,17 @@ for counter in range(40):
     full_msg = b''
     new_msg = True
     while True:
-        msg = s.recv(100)
+        msg = s.recv(100000000)
         if new_msg:
             print(f"New Message Length: {msg[:HEADERSIZE]}")
             msglen = int(msg[:HEADERSIZE])
             new_msg = False 
         
-        full_msg += msg.decode("utf-8")
-
+        full_msg += msg
+        print(len(full_msg))
         if len(full_msg) - HEADERSIZE == msglen:
             print("Done Receiving")
             finalAnswer = np.frombuffer(full_msg[HEADERSIZE:], dtype=np.int64).reshape((1920, 1080, 3))
+            print("DONE")
             new_msg = True
             full_msg = b''
