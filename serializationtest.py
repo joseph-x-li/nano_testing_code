0751535e7751f1159bc7c0bea996f106562b7706
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-04-24 04:22:45 -0400

    amazing serialization test with result @ end

--- serializationtest.py
+++ serializationtest.py
@@ -21,8 +21,25 @@ pickle_dec = pickle.loads(pickle_enc)
 end = time.time()
 pickle_dec_time = end-start
 print("Decoding type: {}".format(type(pickle_dec)))
-print("Encoding time: {}\nDecoding time: {}".format(pickle_enc_time, pickle_dec_time)) 
-assert (fakeFrame == pickle_dec).all()
+print("Encoding time: {}\nDecoding time: {}".format(pickle_enc_time, pickle_dec_time))
+
+fakeFrame = np.random.randint(255, size=(1920, 1080, 3)) # make a fake image of proper size
+
+# tobytes
+print("Now testing: tobytes")
+start = time.time()
+bytes_enc = fakeFrame.tobytes()
+end = time.time()
+bytes_enc_time = end-start
+print("Encoding type: {}".format(type(bytes_enc)))
+
+start = time.time()
+bytes_dec = np.frombuffer(bytes_enc, dtype=np.int64).reshape((1920, 1080, 3))
+end = time.time()
+bytes_dec_time = end-start
+print("Decoding type: {}".format(type(bytes_dec)))
+print("Encoding time: {}\nDecoding time: {}".format(bytes_enc_time, bytes_dec_time)) 
+assert (fakeFrame == bytes_dec).all()
 
 fakeFrame = np.random.randint(255, size=(1920, 1080, 3)) # make a fake image of proper size
 
@@ -59,3 +76,23 @@ tolist_dec_time = end-start
 print("Decoding type: {}".format(type(tolist_dec)))
 print("Encoding time: {}\nDecoding time: {}".format(tolist_enc_time, tolist_dec_time))
 
+# Now testing: pickle
+# Encoding type: <class 'bytes'>
+# Decoding type: <class 'numpy.ndarray'>
+# Encoding time: 0.2712821960449219
+# Decoding time: 0.6469271183013916
+# Now testing: tobytes
+# Encoding type: <class 'bytes'>
+# Decoding type: <class 'numpy.ndarray'>
+# Encoding time: 0.023937702178955078
+# Decoding time: 3.695487976074219e-05
+# Now testing: msgpack-numpy
+# Encoding type: <class 'bytes'>
+# Decoding type: <class 'numpy.ndarray'>
+# Encoding time: 0.055579423904418945
+# Decoding time: 0.02417755126953125
+# Now testing: tolist
+# Encoding type: <class 'list'>
+# Decoding type: <class 'numpy.ndarray'>
+# Encoding time: 2.7683544158935547
+# Decoding time: 4.521432399749756
