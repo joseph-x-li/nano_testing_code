4800f8968fff2acab11794b50a7df17a89f6f0be
jet1 <13826851+joseph-x-li@users.noreply.github.com>
2020-05-22 15:42:42 -0400

    wtf

--- full_code/client.py
+++ full_code/client.py
@@ -4,27 +4,32 @@ import numpy as np
 
 HEADERSIZE = 20
 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(
-    ("172.16.0.38", 11234)
+    ("172.16.0.38", 11238)
 )
 
+
+full_msg = b''
+new_msg = True
+print("MARKK")
 start = time.time()
-for _ in range(40):
-    full_msg = b''
-    new_msg = True
-    while True:
-        msg = s.recv(100_000_000)
-        if new_msg:
-            print(f"New Message Length: {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
-            new_msg = False 
-        
-        full_msg += msg
-        print(len(full_msg))
-        if len(full_msg) - HEADERSIZE == msglen:
-            print("Done Receiving")
-            finalAnswer = np.frombuffer(full_msg[HEADERSIZE:], dtype=np.int64).reshape((1920, 1080, 3))
-            print("DONE")
-            new_msg = True
-            full_msg = b''
+while True:
+    msg = s.recv(10_000_000)
+    if new_msg:
+        print(f"New Message Length: {msg[:HEADERSIZE]}")
+        msglen = int(msg[:HEADERSIZE])
+        new_msg = False 
+    print("mark")
+    full_msg += msg
+    # print(len(full_msg))
+    if len(full_msg) - HEADERSIZE == msglen:
+        end = time.time()
+        print(f"FPS = {1/(end - start)}")
+        print("Done Receiving")
+        # finalAnswer = np.frombuffer(full_msg[HEADERSIZE:], dtype=np.int64).reshape((1920, 1080, 3))
+        print(f"DONE w/ len: {len(full_msg)}")
+        new_msg = True
+        full_msg = b''
+        break
--- full_code/server.py
+++ full_code/server.py
@@ -1,19 +1,33 @@
 import socket
 import numpy as np
+import cv2
 
 HEADERSIZE = 20
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+JET1 = ("172.16.0.38", 11238)
 s.bind(
-    ("172.16.0.38", 11234)
+    JET1
 )
 
 s.listen(5)
+# fakeFrame = np.random.randint(255, size=(1920, 1080, 3)) # make a fake image of proper size
+# fakeFrame = np.zeros(shape = (1920, 1080, 3))
+# is_success, im_buf_arr = cv2.imencode(".jpeg", fakeFrame)
+
+with open("./images/frame0.jpg", "rb") as image:
+  f = image.read()
+  b = bytearray(f)
+
+# msg = im_buf_arr.tobytes()
+msg = b
+msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
 
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} is established!")
-    fakeFrame = np.random.randint(255, size=(1920, 1080, 3)) # make a fake image of proper size
-    msg = fakeFrame.tobytes()
-    msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
-    clientsocket.send(msg)
+    # with open('./images/frame0.jpg', 'rb') as f:
+    #     clientsocket.sendfile(f, 0)
+    # clientsocket.close()
+    clientsocket.sendall(msg)
