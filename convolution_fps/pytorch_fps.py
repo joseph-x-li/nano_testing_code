86148bd810c59f0303f09d7c7501827af7b909a6
YRUDancin <13826851+joseph-x-li@users.noreply.github.com>
2020-05-30 22:46:05 -0700

    formatting

--- convolution_fps/pytorch_fps.py
+++ convolution_fps/pytorch_fps.py
@@ -4,21 +4,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-bs=1   #batch size
-xdim = 64
-ydim = 64
-kernel_len = 5 #1, 3, 5, 7
-input_shape = (bs, 3, xdim, ydim)
-
 #https://pytorch.org/docs/master/nn.html#torch.nn.Conv2d
 
 class ConvNet(nn.Module):
+  bs = 1    # batch size
+  xdim = 64
+  kernel_len = 5 # 1, 3, 5, 7
+  input_shape = (bs, 3, xdi, ydim)
   def __init__(self):
     super(ConvNet, self).__init__()
     #nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-    self.layer1 = nn.Conv2d(3, 32, kernel_len, stride=1)
-    self.layer2 = nn.Conv2d(32, 64, kernel_len, stride=1)
-    self.layer3 = nn.Conv2d(64, 64, kernel_len, stride=1)
+    self.layer1 = nn.Conv2d(3, 32, self.kernel_len, stride=1)
+    self.layer2 = nn.Conv2d(32, 64, self.kernel_len, stride=1)
+    self.layer3 = nn.Conv2d(64, 64, self.kernel_len, stride=1)
 
 
   def forward(self, x):
@@ -53,11 +51,15 @@ def testFPS(model, n, device_on):
   print("FPS: ", n/total_time)
   return total_time
 
+
+n = 1000 #number of times we repeat the prediction
+
 def main():
   use_cuda = True
   device = torch.device("cuda" if use_cuda else "cpu")
   model = ConvNet().to(device)
   testFPS(model, n, device)
 
-n = 1000 #number of times we repeat the prediction
-main()
+if __name__ == "__main__":
+  main()
+
